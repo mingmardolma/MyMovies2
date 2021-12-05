d6912b7d2069c36518ed381a9d8dd5e8350ac7db
Mingmar Dolma <62723387+mingmardolma@users.noreply.github.com>
2021-12-05 17:51:40 -0500

    Add files via upload

---
+++ MyMovie/MovieGridCell.swift
@@ -0,0 +1,15 @@
+//
+//  MovieGridCell.swift
+//  MyMovie
+//
+//  Created by Dawa Sonam on 12/4/21.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
